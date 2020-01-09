9141de629d309aeb737b168b56942a96ab1dd6b9
Fiona Corden <fiona.corden@metaswitch.com>
2020-01-09 11:58:30 +0000

    Fix up program that was broken in refactor

--- src/five.rs
+++ src/five.rs
@@ -13,7 +13,7 @@ pub fn run_b() {
     // Read the input file as a string and create computer.
     let mut intcomp = Intcomp::from_file("inputs/input_5.txt");
 
-    intcomp.input.push(1);
+    intcomp.input.push(5);
     let result = intcomp.execute();
     println!("5b Output is: {:?}", result.get_output());
 }
